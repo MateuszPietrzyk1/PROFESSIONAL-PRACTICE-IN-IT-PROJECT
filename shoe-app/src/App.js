8a841a881bf47c09fc56b3e5a4ef2d7a53410b8c
davidamankwah <G00394825@gmit.ie>
2023-02-10 16:05:04 +0000

    add

---
+++ shoe-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+        <h2>hi loog</h2>
+        <h2>It is </h2>
+      </div>
+    );
+  }
+}
+export default App;
